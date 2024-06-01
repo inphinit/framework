b36d62c3d651582d09bfa9aca9b697a266d3ceec
Guilherme Nascimento <brcontainer@yahoo.com.br>
2024-06-01 09:16:51 -0300

    Update App.php

--- src/Inphinit/App.php
+++ src/Inphinit/App.php
@@ -16,7 +16,7 @@ use Inphinit\Viewing\View;
 class App
 {
     /** Inphinit framework version */
-    const VERSION = '0.6.0';
+    const VERSION = '0.6.2';
 
     private static $events = array();
     private static $configs = array();
