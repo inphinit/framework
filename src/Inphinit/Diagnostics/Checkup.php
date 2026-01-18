8606d7d32fe0abaa086cd26a44de75b8edbc2cb8
Guilherme Nascimento <brcontainer@yahoo.com.br>
2026-01-17 23:54:28 -0300

    Update Checkup.php
    New features:
    
    - Check server memory settings
    - Check settings for POST and upload

--- src/Inphinit/Diagnostics/Checkup.php
+++ src/Inphinit/Diagnostics/Checkup.php
@@ -9,13 +9,10 @@
 
 namespace Inphinit\Diagnostics;
 
-use Inphinit\Dom\Document;
-use Inphinit\Exception;
-
 class Checkup
 {
-    const AGE_CHECK = 1;
-    const AGE_LEGACY = 2;
+    const SUPPORT_ACTIVE = 80400;
+    const SUPPORT_SECURITY_ONLY = 80200;
 
     private $iniPath = '';
     private $iniGet = true;
@@ -25,50 +22,14 @@ class Checkup
     private $errors = array();
     private $warnings = array();
 
-    private $buildAge;
-    private static $buildDate;
-
     const MSG_INI_CONFIGS = '`%s`, additional `.ini` files, or via directives';
     const MSG_DEV_ADVICE = 'While in development mode, it is recommended to disable `%s` in %s';
-
-    const CACHE_PHP_BUILD_DATE = '.PHP_BUILD_DATE';
 
     public function __construct()
     {
         if (\Inphinit\App::config('environment') === 'development') {
             $this->development = true;
             $this->sensitive = true;
-        }
-
-        if ($this->sensitive && ($buildAge = $this->getBuildAge())) {
-            $version = PHP_VERSION;
-
-            if ($buildAge > self::AGE_LEGACY) {
-                $this->errors[] = "PHP{$version} is more than {$buildAge} years old — upgrading to a " .
-                                  "newer version is strongly recommended for security and performance reasons";
-            } elseif ($buildAge > self::AGE_CHECK) {
-                $this->warnings[] = "PHP{$version} has not received updates for over {$buildAge} years — " .
-                                    "consider applying the latest security patches or upgrading to a newer release";
-            }
-        }
-
-        if (function_exists('ini_get') === false) {
-            $this->warnings[] = 'The `ini_get` function is disabled, so no further checks can be performed';
-            $this->iniGet = false;
-        }
-
-        if (function_exists('php_ini_loaded_file')) {
-            $this->iniPath = php_ini_loaded_file();
-
-            if ($this->iniPath) {
-                $this->checkRandomBytes();
-                $this->collectErrors();
-                $this->collectWarnings();
-            } else {
-                $this->warnings[] = '`php.ini` is not configured or could not be located on this server';
-            }
-        } else {
-            $this->warnings[] = 'The `php_ini_loaded_file()` is disabled, so no further checks can be performed';
         }
     }
 
@@ -103,69 +64,116 @@ class Checkup
     }
 
     /**
-     * Get PHP build date (backward compatibility support)
-     *
-     * @throws \Inphinit\Exception
-     * @return string
+     * Excute the checkup
      */
-    public static function getBuildDate()
+    public function exec()
     {
-        if (defined('PHP_BUILD_DATE')) {
-            return PHP_BUILD_DATE;
-        } elseif (self::$buildDate === null) {
-            $cache = INPHINIT_SYSTEM . '/storage/' . self::CACHE_PHP_BUILD_DATE;
+        $this->errors = array();
+        $this->warnings = array();
 
-            if (is_file($cache)) {
-                self::$buildDate = file_get_contents($cache);
-            } elseif (function_exists('phpinfo')) {
-                ob_start();
+        if ($this->sensitive) {
+            $version = PHP_VERSION;
 
-                phpinfo(INFO_GENERAL);
-
-                $handle = new Document(Document::HTML);
-                $handle->load(ob_get_clean());
-
-                $node = $handle->selector()->first('td:contains(Build Date)+td');
-
-                if ($node && $value = trim($node->textContent)) {
-                    self::$buildDate = $value;
-                    file_put_contents($cache, $value);
-                } else {
-                    throw new Exception('The PHP release date could not be determined (missing build date in phpinfo())');
-                }
-            } else {
-                throw new Exception('The PHP release date could not be determined (phpinfo() is disabled)');
+            if (PHP_VERSION_ID < self::SUPPORT_SECURITY_ONLY) {
+                $this->errors[] = "PHP {$version} is no longer supported. " .
+                                  'Updating as soon as possible is strongly recommended';
+            } elseif (PHP_VERSION_ID < self::SUPPORT_ACTIVE) {
+                $this->warnings[] = "PHP {$version} continues to receive security updates; " .
+                                    'however, upgrading is recommended to benefit from further improvements';
             }
         }
 
-        return self::$buildDate;
-    }
-
-    private function getBuildAge()
-    {
-        if ($this->buildAge !== null) {
-            return $this->buildAge;
+        if (function_exists('ini_get') === false) {
+            $this->warnings[] = 'The `ini_get` function is disabled, so no further checks can be performed';
+            $this->iniGet = false;
         }
 
-        $date = self::getBuildDate();
+        if (function_exists('php_ini_loaded_file')) {
+            $this->iniPath = php_ini_loaded_file();
 
-        $age = false;
-
-        if ($date) {
-            try {
-                $current = new \DateTime();
-                $build = new \DateTime($date);
-                $age = $current->diff($build)->y;
-            } catch (\Exception $e) {
-                $this->warnings[] = "The PHP release date could not be determined (invalid date: {$date})";
+            if ($this->iniPath) {
+                $this->checkMemory();
+                $this->checkPost();
+                $this->checkRandomBytes();
+                $this->collectErrors();
+                $this->collectWarnings();
+            } else {
+                $this->warnings[] = '`php.ini` is not configured or could not be located on this server';
             }
         } else {
-            $this->warnings[] = 'The PHP release date could not be determined';
+            $this->warnings[] = 'The `php_ini_loaded_file()` is disabled, so no further checks can be performed';
+        }
+    }
+
+    private function checkMemory()
+    {
+        $directives = $this->getDirectives();
+
+        $memory_limit_entry = ini_get('memory_limit');
+
+        if ($memory_limit_entry === '-1') {
+            $memory_limit = -1;
+        } else {
+            $memory_limit = self::convertSize($memory_limit_entry, '128M');
         }
 
-        $this->buildAge = $age;
+        if ($memory_limit === false) {
+            $this->errors[] = "Invalid value in entry `memory_limit={$memory_limit_entry}`. " .
+                              "Adjustment in {$directives}";
+        } elseif ($memory_limit === -1) {
+            $this->errors[] = "Unlimited memory (`memory_limit=-1`) is problematic. Adjustment in {$directives}";
+        } elseif ($memory_limit < 16 * 1024 * 1024) {
+            $this->warnings[] = "`memory_limit={$memory_limit_entry}` may not be enough. " .
+                                "Adjustment in {$directives}";
+        }
+    }
 
-        return $age;
+    private function checkPost()
+    {
+        $directives = $this->getDirectives();
+
+        $post_max_size_entry = ini_get('post_max_size');
+        $upload_max_filesize_entry = ini_get('upload_max_filesize');
+
+        if ($post_max_size_entry === '') {
+            $post_max_size_entry = '2M';
+        }
+
+        if ($upload_max_filesize_entry === '') {
+            $upload_max_filesize_entry = '8M';
+        }
+
+        $min_size = 1024 * 1024;
+        $post_max_size = self::convertSize($post_max_size_entry, '8M');
+
+        if ($post_max_size === false) {
+            $this->errors[] = "Invalid value in entry `post_max_size={$post_max_size_entry}`. " .
+                              "Adjustment in {$directives}";
+        } elseif ($post_max_size < $min_size) {
+            $this->errors[] = "`post_max_size={$post_max_size_entry}` is very small. " .
+                              "Adjustment in {$directives}";
+        }
+
+        if (self::isEnabled('file_uploads')) {
+            $upload_max_filesize = self::convertSize($upload_max_filesize_entry, '2M');
+
+            if ($upload_max_filesize === false) {
+                $this->errors[] = "Invalid value in entry `upload_max_filesize={$upload_max_filesize_entry}`. " .
+                                  "Adjustment in {$directives}";
+            }
+
+            if ($post_max_size < $upload_max_filesize) {
+                $this->errors[] = "`post_max_size={$post_max_size_entry}` is smaller than " .
+                                  "`upload_max_filesize={$upload_max_filesize_entry}`. " .
+                                  "Adjustment in {$directives}";
+            } elseif ($upload_max_filesize < $min_size) {
+                $this->errors[] = "`upload_max_filesize={$upload_max_filesize_entry}` is very small. " .
+                                  "Adjustment in {$directives}";
+            }
+        } elseif (PHP_SAPI !== 'cli') {
+            $this->warnings[] = 'File uploads are disabled. If this is not intentional' .
+                                ', enable `file_uploads=On` in ' . $directives;
+        }
     }
 
     private function checkRandomBytes()
@@ -174,7 +182,7 @@ class Checkup
 
         if (function_exists('random_bytes') === false) {
             if (function_exists('openssl_random_pseudo_bytes')) {
-                $this->warnings[] = '`random_bytes()` is not available; OpenSSL is enabled and may be used as a fallback';
+                $this->warnings[] = '`random_bytes()` unavailable. Using OpenSSL as a fallback';
             } elseif (PHP_VERSION_ID < 70000) {
                 $this->errors[] = '`random_bytes()` polyfill is required or enable OpenSSL extension in ' . $directives;
             } else {
@@ -192,12 +200,26 @@ class Checkup
             $this->errors[] = 'Disable `magic_quotes_gpc` in ' . $directives;
         }
 
-        if ($this->iniGet && PHP_VERSION_ID < 70000 && self::isEnabled('always_populate_raw_post_data')) {
-            $this->errors[] = 'Set -1 to `always_populate_raw_post_data` in ' . $directives;
-        }
+        if ($this->iniGet) {
+            if (PHP_VERSION_ID < 70000 && self::isEnabled('always_populate_raw_post_data')) {
+                $this->errors[] = 'Set -1 to `always_populate_raw_post_data` in ' . $directives;
+            }
 
-        if ($this->iniGet && $this->development === false && self::isEnabled('display_errors')) {
-            $this->errors[] = 'In production environment, the `display_errors` must be disabled in ' . $directives;
+            if ($this->development === false && self::isEnabled('display_errors')) {
+                $this->errors[] = 'In production environment, the `display_errors` must be disabled in ' . $directives;
+            }
+
+            if (PHP_SAPI !== 'cli') {
+                $max_execution_time = ini_get('max_execution_time');
+
+                if ($max_execution_time < 1) {
+                    $this->errors[] = 'In a web context, an unlimited `max_execution_time` is unsafe. ' .
+                                      'Adjustment in ' . $directives;
+                } elseif ($max_execution_time > 300) {
+                    $this->errors[] = 'In a web context, `max_execution_time` should typically be limited to 30–300 ' .
+                                      'seconds. Adjustment in ' . $directives;
+                }
+            }
         }
 
         $folder = INPHINIT_SYSTEM . '/storage';
@@ -219,7 +241,8 @@ class Checkup
         }
 
         if ($this->iniGet && self::isEnabled('auto_detect_line_endings')) {
-            $this->warnings[] = '`auto_detect_line_endings` is deprecated as of PHP 8.1.0, set 0 or remove in ' . $directives;
+            $this->warnings[] = '`auto_detect_line_endings` is deprecated as of PHP 8.1.0,' .
+                                ' set 0 or remove in ' . $directives;
         }
 
         if ($this->development && $this->iniGet) {
@@ -237,12 +260,22 @@ class Checkup
                 $this->warnings[] = sprintf(self::MSG_DEV_ADVICE, 'xcache.cacher', $directives);
             }
         }
+
+        if ($this->iniGet && self::isEnabled('expose_php')) {
+            $message = 'Set `expose_php=Off` to ensure the PHP version is not exposed through HTTP headers';
+
+            if (PHP_VERSION_ID < 505000) {
+                $message .= ' or PHP easter eggs';
+            }
+
+            $this->warnings[] = $message. ' in ' . $directives;
+        }
     }
 
     private static function isEnabled($key)
     {
         $value = ini_get($key);
-        return $value ? in_array(strtolower($value), array('on', '1', 'yes')) : false;
+        return $value ? in_array(strtolower($value), array('on', '1', 'yes', 'true')) : false;
     }
 
     private function getDirectives()
@@ -252,5 +285,25 @@ class Checkup
         }
 
         return sprintf(self::MSG_INI_CONFIGS, 'php.ini');
+    }
+
+    private static function convertSize($entry, $default)
+    {
+        if ($entry === '') {
+            $entry = $default;
+        }
+
+        if (preg_match('/^(\d+?)([KMG]|)$/i', $entry, $matches) !== 1) {
+            return false;
+        }
+
+        $value = intval($matches[1]);
+        $shorthand = $matches[2];
+
+        if ($shorthand) {
+            $value *= pow(1024, stripos('_KMG', $shorthand));
+        }
+
+        return $value;
     }
 }
